6b154f873dadaf0cabb8cba2b58eb0f62d126739
Matthew Gong <matthew.zhigang.gong@gmail.com>
2020-10-19 23:00:13 +1300

    modify db design to adapt to the daily quote scraping

--- doc/mysql/sql/utils/basic_query.sql
+++ doc/mysql/sql/utils/basic_query.sql
@@ -14,12 +14,13 @@ select count(*) from rental_route;
 select * from daily_quote_scraping_task;
 delete from daily_quote_scraping_task t where t.id > 0;
 set @debug_enabled = false;
-call add_daily_quote_scraping_task(@new_daily_quote_scraping_task_id);
-select @new_daily_quote_scraping_task_id;
+call add_todays_quote_scraping_task(@todays_quote_scraping_task_id);
+select @todays_quote_scraping_task_id;
 
 select * from booking_request;
 select count(*) from booking_request;
 delete from booking_request t where t.id > 0;
+call get_pending_booking_requests(0, 10);
 
 select * from rental_quote;
 select distinct booking_request_id from rental_quote;
@@ -32,5 +33,5 @@ set @debug_enabled = true;
 call clear_data_of_company(2, true);       
 call delete_empty_daily_quote_scraping_tasks();
 
-call get_pending_booking_requests(0, 10);
+
 
