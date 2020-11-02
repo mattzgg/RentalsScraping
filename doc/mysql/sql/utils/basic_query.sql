df8e43814ce9547a8f12fa8af12ef328568066c9
Matthew Gong <matthew.zhigang.gong@gmail.com>
2020-11-03 12:48:18 +1300

    Add stored routines for handling booking requests

--- doc/mysql/sql/utils/basic_query.sql
+++ doc/mysql/sql/utils/basic_query.sql
@@ -2,39 +2,27 @@ set time_zone = '+13:00';
 SELECT @@global.time_zone, @@session.time_zone;
 select utc_timestamp();
 select now();
-select date_format(curdate(), '%d/%m/%Y %H:%i');
+select date_format(curdate(), '%d/%m/%Y %h:%i %p');
 
 select * from company order by id;
 select * from company limit 0, 1;
-select * from location;
+select * from location order by id;
 select * from office;
-select * from raw_rental_route;
 select * from rental_route;
 select count(*) from rental_route;
+select * from company_rental_route;
 
-select * from daily_quote_scraping_task;
-delete from daily_quote_scraping_task t where t.id > 0;
-set @debug_enabled = false;
-call add_todays_quote_scraping_task(@todays_quote_scraping_task_id);
-select @todays_quote_scraping_task_id;
+select id, value from pick_up_date;
+select id, date_format(value, '%d/%m/%Y %h:%i %p') value from pick_up_date;
+call get_booking_request_statistics('03/11/2020', @total_count, @fulfilled_count);
+select @total_count, @fulfilled_count;
 
-select * from booking_request;
-select count(*) from booking_request;
-delete from booking_request t where t.id > 0;
-call get_todays_pending_booking_requests(0, 10);
-call get_todays_booking_request_statistics(@tbrs_total, @tbrs_pending_count);
-select @tbrs_total, @tbrs_pending_count;
+select * from rental_route order by id;
+call get_non_fulfilled_booking_requests('03/11/2020', 1000, 1000);
 
-select * from rental_quote;
-select distinct booking_request_id from rental_quote;
 
-set @debug_enabled = false;
-call add_daily_quote_scraping_task(@record_id);
-select @record_id;
 
-set @debug_enabled = true;
-call clear_data_of_company(2, true);       
-call delete_empty_daily_quote_scraping_tasks();
+
 
 
 
